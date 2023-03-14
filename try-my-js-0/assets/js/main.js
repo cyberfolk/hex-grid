caa1c1aaf4098624cb16986089338541f414093b
cyberfolk <andrea1995ferrari.com>
2023-03-14 11:46:13 +0100

    Initial commit [jsnack1]

--- try-my-js-0/assets/js/main.js
+++ try-my-js-0/assets/js/main.js
@@ -1,8 +1,8 @@
 const CENTER = 50;
-const X_SHIFT = 12.5;
-const Y_SHIFT = 18.75;
-const X_ARRAY = [0, -2, -1, 1, 2, 1, -1, -4, -3, -2, 0, 2, 3, 4, 3, 2, 0, -2, -3];
-const Y_ARRAY = [0, 0, 1, 1, 0, -1, -1, 0, 1, 2, 2, 2, 1, 0, -1, -2, -2, -2, -1];
+const X_SHIFT = 20;
+const Y_SHIFT = 10;
+const X_ARRAY = [0, 0, 1, 1, 0, -1, -1, 0, 1, 2, 2, 2, 1, 0, -1, -2, -2, -2, -1];
+const Y_ARRAY = [0, -2, -1, 1, 2, 1, -1, -4, -3, -2, 0, 2, 3, 4, 3, 2, 0, -2, -3];
 
 const containerEl = document.querySelector(".container");
 
@@ -12,8 +12,8 @@ qhexElement.classList.add("qhex");
 for (let i = 0; i < 19; i++) {
   const hexElement1 = document.createElement("div");
   hexElement1.classList.add("hex");
-  hexElement1.style.top = CENTER + X_ARRAY[i] * X_SHIFT + "%";
-  hexElement1.style.left = CENTER + Y_ARRAY[i] * Y_SHIFT + "%";
+  hexElement1.style.top = CENTER + Y_ARRAY[i] * Y_SHIFT + "%";
+  hexElement1.style.left = CENTER + X_ARRAY[i] * X_SHIFT + "%";
   hexElement1.innerText = i + 1;
   qhexElement.append(hexElement1);
 }
