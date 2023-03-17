2ea99f4fb2d26ee325e2ec6d249bfd88194b8e24
cyberfolk <andrea1995ferrari.com>
2023-03-17 01:40:30 +0100

    replace qhex with quadrant

--- try-my-js-0/assets/js/main.js
+++ try-my-js-0/assets/js/main.js
@@ -1,13 +1,13 @@
 const CENTER = 50;
-const X_SHIFT = 20;
+const X_SHIFT = 6.25;
 const Y_SHIFT = 10;
-const X_ARRAY = [0, 0, 1, 1, 0, -1, -1, 0, 1, 2, 2, 2, 1, 0, -1, -2, -2, -2, -1];
+const X_ARRAY = [0, 0, 3, 3, 0, -3, -3, 0, 3, 6, 6, 6, 3, 0, -3, -6, -6, -6, -3];
 const Y_ARRAY = [0, -2, -1, 1, 2, 1, -1, -4, -3, -2, 0, 2, 3, 4, 3, 2, 0, -2, -3];
 
 const containerEl = document.querySelector(".container");
 
 const qhexElement = document.createElement("div");
-qhexElement.classList.add("qhex");
+qhexElement.classList.add("quadrant");
 
 for (let i = 0; i < 19; i++) {
   const hexElement1 = document.createElement("div");
