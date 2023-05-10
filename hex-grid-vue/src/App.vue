bb64ce838344e205156ad94c8cc8f3b46c571cbf
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-11 00:49:18 +0200

    init commit

---
+++ hex-grid-vue/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+export default {
+    components: {
+        SiteHeader,
+        SiteMain,
+        SiteFooter,
+    },
+}
+</script>
+
+<template>
+    <SiteHeader></SiteHeader>
+    <SiteMain></SiteMain>
+    <SiteFooter></SiteFooter>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
---
+++ hex-grid-vue/src/components/SiteMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "SiteMain",
+  components: {},
+};
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss';
+</style>
